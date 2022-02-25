8ad8280f61096972b6940b7ab3255ead2da60879
Aakashsethi <aakash.sethi7@gmail.com>
2022-02-25 01:54:19 -0500

    Creating a Repository Layer

---
+++ src/main/java/com/xadmin/SprinBootCrud/repository/SubjectRepository.java
@@ -0,0 +1,9 @@
+package com.xadmin.SprinBootCrud.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.xadmin.SprinBootCrud.bean.Subject;
+
+public interface SubjectRepository extends CrudRepository<Subject,String> {
+
+}
